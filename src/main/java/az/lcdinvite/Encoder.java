803acbd6ce0ceeb8684272101667221d701e29e0
AlexZam <alexzzam@gmail.com>
2012-02-16 17:27:49 +0400

    Dialog customize

---
+++ src/main/java/az/lcdinvite/Encoder.java
@@ -0,0 +1,11 @@
+package az.lcdinvite;
+
+public class Encoder {
+    public static String encode(String text) {
+        return text;
+    }
+
+    public static String decode(String text) {
+        return text;
+    }
+}
--- src/main/java/az/lcdinvite/MainForm.java
+++ src/main/java/az/lcdinvite/MainForm.java
@@ -1,20 +1,36 @@
 package az.lcdinvite;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
-    private JButton buttonCancel;
+    private JButton btClose;
+    private JTextArea taSource;
+    private JTextArea taEncoded;
+    private JTextArea taCode;
+    private JButton encodeButton;
+    private JButton decodeButton;
+    private static MainForm dialog;
 
     public MainForm() {
+        setResizable(false);
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        btClose.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dialog.setVisible(false);
+                dialog.dispose();
+            }
+        });
     }
 
     public static void main(String[] args) {
-        MainForm dialog = new MainForm();
+        dialog = new MainForm();
         dialog.pack();
         dialog.setVisible(true);
         System.exit(0);
